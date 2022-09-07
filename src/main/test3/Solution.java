e0aa238a645b7d7474b5fca04f21715e5476e3be
Ivangorno <111599129+Ivangorno@users.noreply.github.com>
2022-09-07 22:53:33 +0300

    added Regex check to getRoman()

--- src/main/test3/Solution.java
+++ src/main/test3/Solution.java
@@ -1,16 +1,16 @@
 package main.test3;
 
-import java.util.HashMap;
-import java.util.Scanner;
+import java.util.*;
 
 public class Solution {
     public static void main(String[] args) {
 
-        String romanNumeral = getRoman();
-        RomanConverter(romanNumeral);
+        RomanConverter(getRoman());
     }
 
     public static void RomanConverter(String roman) {
+
+
 
         HashMap<Character, Integer> map = new HashMap<Character, Integer>();
         map.put('I', 1);
@@ -21,28 +21,34 @@ public class Solution {
         map.put('D', 500);
         map.put('M', 1000);
 
-        roman.replace("IV", "IIII");
-        roman.replace("IX", "IIIIIIIII");
-        roman.replace("XL", "XXXX");
-        roman.replace("XC", "XXXXXXXXX");
-        roman.replace("CD", "CCCC");
-        roman.replace("CM", "CCCCCCCCC");
+        roman = roman.replace("IV", "IIII");
+        roman = roman.replace("IX", "VIIII");
+        roman = roman.replace("XL", "XXXX");
+        roman = roman.replace("XC", "XXXXXXXXX");
+        roman = roman.replace("CD", "CCCC");
+        roman = roman.replace("CM", "CCCCCCCCC");
 
         int number = 0;
 
         for (int i = 0; i < roman.length(); i++) {
-            number = number + map.get(roman.charAt(i));
+            number = number + (map.get(roman.charAt(i)));
         }
 
         System.out.println("Your number converted from Roman is: " + number);
     }
 
     static String getRoman() {
-
-        Scanner input = new Scanner(System.in);
         System.out.println("Enter a Roman numeral:  ");
+        Scanner input = new Scanner(System.in);
+        String inputString = input.nextLine();
 
-        return input.nextLine();
+        while (!inputString.matches("[IVXLCDM]+")){
+            System.out.println("ERROR: invalid Roman numeral entered ");
+            System.out.println("Please enter a ROMAN NUMERAL:  ");
+            inputString=input.nextLine();
+        }
+
+        return inputString;
     }
 
 
